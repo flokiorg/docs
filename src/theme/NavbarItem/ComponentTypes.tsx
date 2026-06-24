c5022d0a2555236e9b0f7610aa655bd37c1c5207
mechion <4387895+mechion@users.noreply.github.com>
2026-06-24 14:37:55 +0000

    feat: register custom AskAI navbar item type

---
+++ src/theme/NavbarItem/ComponentTypes.tsx
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import AskAINavbarItem from '@site/src/components/AskAI';
+
+export default {
+  ...ComponentTypes,
+  'custom-askAI': AskAINavbarItem,
+};
